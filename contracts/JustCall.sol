a06e7278c141fcba7146f0beca31cf3493d6e587
nigelgks <nigelgan@yahoo.com>
2024-08-16 16:34:48 +0800

    Updated smart contract JustCall.sol

--- contracts/JustCall.sol
+++ contracts/JustCall.sol
@@ -2,16 +2,58 @@
 pragma solidity ^0.8.24;
 
 contract JustCall {
+    address public owner;
+    
     struct Profile {
         address addr;
         string fullName;
         string phoneNumber;
     }
 
-    mapping(string => string fullName) public users;
+    mapping(string => string fullName) private users;
     mapping(address => Profile) private profile;
 
-    function register(string calldata _fullName, string calldata _phoneNumber) external {
+    //Initialize contract owner
+    constructor() {
+        owner = msg.sender;
+    }
+
+    //Validate owner address
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the contract administrator can call this function.");
+        _;
+    }
+
+    //Validate new user
+    modifier onlyNewUser(string calldata _phoneNumber) {
+        require(profile[msg.sender].addr == address(0), "User already registered.");
+        require(bytes(users[_phoneNumber]).length == 0, "Phone number already registered");
+        _;
+    }
+
+    //Validate phone number format
+    modifier validatePhoneNum(string calldata _phoneNumber) {
+        require(bytes(_phoneNumber).length == 10, "Invalid phone number length.");
+        _;
+    }
+
+    //Validate phone number exist
+    modifier validateNumberLookup(string calldata _phoneNumber) {
+        require(bytes(users[_phoneNumber]).length != 0, "Phone number is not registered.");
+        _;
+    }
+
+    //Validate address exist
+    modifier validateAddressLookup {
+        require(profile[msg.sender].addr != address(0), "User does not exist.");
+        _;
+    }
+
+    //Registration for new user
+    function register(
+        string calldata _fullName,
+        string calldata _phoneNumber
+    ) external onlyNewUser(_phoneNumber) validatePhoneNum(_phoneNumber) {
         profile[msg.sender] = Profile({
             addr: msg.sender,
             fullName: _fullName,
@@ -21,13 +63,31 @@ contract JustCall {
         users[_phoneNumber] = _fullName;
     }
 
-    function getUserByPhoneNumber(string calldata _phoneNumber) external view returns(string memory, string memory phoneNumber) {
+    //Get full name from phone number lookup
+    function getUserByPhoneNumber(
+        string calldata _phoneNumber
+    ) external validatePhoneNum(_phoneNumber) validateNumberLookup(_phoneNumber) view returns(
+        string memory,
+        string memory phoneNumber
+    ) {
         string memory fullName = users[_phoneNumber];
         return (fullName, _phoneNumber);
     }
 
-    function getUserByAddress() external view returns(string memory fullName, string memory phoneNumber, address addr) {
+    //Get profile details from user address
+    function getUserByAddress() external validateAddressLookup view returns(
+        string memory fullName,
+        string memory phoneNumber,
+        address addr
+    ) {
         Profile storage user = profile[msg.sender];
         return (user.fullName, user.phoneNumber, user.addr);
+    }
+
+    //Remove profile when requested (owner only)
+    function removeUser(address _addr) external onlyOwner {
+        Profile storage user = profile[_addr];
+        delete users[user.phoneNumber];
+        delete profile[_addr];
     }
 }
